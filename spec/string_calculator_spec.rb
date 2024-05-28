fc580dac48f9c51cb5b4364a3716101d9aed96ff
rishabhmatta20 <rishabhmatta20@gmail.com>
2024-05-28 16:48:24 +0530

    Add spec to return the sum of multiple numbers in a string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -14,5 +14,9 @@ RSpec.describe StringCalculator do
     it 'returns the number itself for a single number' do
       expect(StringCalculator.add("1")).to eq(1)
     end
+
+    it 'returns the sum of multiple numbers' do
+      expect(StringCalculator.add("1,2,3")).to eq(6)
+    end
   end
 end
