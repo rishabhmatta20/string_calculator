f78b66bf08be49ecfb54f74967c8eb3347be16e1
rishabhmatta20 <rishabhmatta20@gmail.com>
2024-05-28 17:34:30 +0530

    Handle exception for negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,18 +2,22 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
 
-    numbers, delimiters = numbers_and_its_delimiters(numbers)
-    numbers.split(delimiters).map(&:to_i).sum
+    numbers = split_delimiters(numbers)
+
+    negatives = numbers.select { |num| num < 0 }
+    raise "negative numbers not allowed: #{negatives.join(', ')}" unless negatives.empty?
+
+    numbers.sum
   end
 
   private
 
-  def self.numbers_and_its_delimiters(numbers)
+  def self.split_delimiters(numbers)
     if numbers.start_with?("//")
       delimiter, numbers = numbers.split("\n", 2)
-      [numbers, delimiter[2..-1]]
+      numbers.split(delimiter[2..-1]).map(&:to_i)
     else
-      [numbers, /,|\n/]
+      numbers.split(/,|\n/).map(&:to_i)
     end
   end
 end
